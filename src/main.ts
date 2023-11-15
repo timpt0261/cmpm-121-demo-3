4ff8d698863e89abce4987bc797135dc26813e42
timpt0261 <rchavez0215@gmail.com>
2023-11-14 16:36:58 -0800

    Completed Step 3.c

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ leaflet
   .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      "&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
   })
   .addTo(map);
 
